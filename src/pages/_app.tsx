413d934eaab7b8a26979b7324a84bbf9ab3bf753
Luiz Rioja <lulyrioja@gmail.com>
2022-05-20 20:52:30 -0300

    chore(App): add auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import { AppProps } from "next/app";
-
+import { SessionProvider as NextAuthProvider } from "next-auth/react";
 import "../../styles/global.scss";
 import { Header } from "../components/Header";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
