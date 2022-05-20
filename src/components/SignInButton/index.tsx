a691611be386f87837d04633c86731fa0298db74
Luiz Rioja <lulyrioja@gmail.com>
2022-05-20 20:53:06 -0300

    feat(SignInButton): implement login with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,28 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04D361" />
-      Luiz Rioja
+      {session.user.name}
       <FiX className={styles.closeIcon} color="#737380" />
     </button>
   ) : (
-    <button className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#EBA417" />
       Sign in with Github
     </button>
