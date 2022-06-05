a37dfc63ed72f7403f092ffe36909ecc94a55ff4
Luiz Rioja <lulyrioja@gmail.com>
2022-06-04 23:39:19 -0300

    chore: add slice header

---
+++ .slicemachine/assets/slices/Header/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Header';
+
+export default {
+  title: 'slices/Header'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"header","items":[],"primary":{"title":[{"type":"heading1","text":"Deploy distributed web services","spans":[]}],"description":[{"type":"paragraph","text":"Ea ut ipsum duis Lorem dolor commodo officia voluptate voluptate dolore ut ipsum nisi cillum et. Exercitation exercitation Lorem veniam eiusmod velit esse sit ad ullamco. Irure cillum aliquip laboris dolor deserunt eiusmod est anim amet.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
